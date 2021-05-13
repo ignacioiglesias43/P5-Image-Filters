54ebcfe67631d3c996afbd7d5cbc749808680095
Ignacio Iglesias Campoy <iiglesiascampoy@gmail.com>
2021-05-13 00:57:35 -0600

    first commit

---
+++ main/sketch.js
@@ -0,0 +1,14 @@
+let img;
+
+function preload() {
+  img = loadImage("./assets/zoro.jpeg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  clear();
+  image(img, 0, 0, windowWidth, img.height);
+}
